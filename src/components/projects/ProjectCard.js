14bdd0bcdfbffc7e1e7bcc0e7a19893573f019fb
Debajit Mallick <mallickdebajit3@gmail.com>
2023-05-11 10:11:43 +0530

    fix project section logo cut issue

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -4,6 +4,7 @@ import {
   Card,
   CardContent,
   CardMedia,
+  Grid,
   Typography,
 } from '@mui/material';
 import { Link } from '@mui/icons-material';
@@ -14,46 +15,49 @@ const ProjectCard = ({ title, description, image, webURL, githubURL }) => {
     <>
       <Card className='mb-3'>
         <CardContent className='d-flex'>
-          <CardMedia
-            image={image}
-            component='img'
-            sx={{ maxWidth: 130, borderRadius: 5 }}
-            alt={`${title} logo`}
-            loading='lazy'
-          />
-          <Box className='ms-3 d-flex flex-column pt-1'>
-            <Typography variant='h4' component='h1'>
-              {title}
-            </Typography>
-            <Typography>
-              <span className='fw-bold'>Description: </span>
-              {description}
-            </Typography>
-            <Box>
-              <Button
-                endIcon={<Link />}
-                variant='contained'
-                href={webURL}
-                target='_blank'
-                sx={{
-                  m: 1,
-                }}
-              >
-                Website
-              </Button>
-              <Button
-                endIcon={<Link />}
-                variant='contained'
-                href={githubURL}
-                target='_blank'
-                sx={{
-                  m: 1,
-                }}
-              >
-                GitHub
-              </Button>
-            </Box>
-          </Box>
+          <Grid container>
+            <Grid sm={2}>
+              <CardMedia
+                image={image}
+                component='img'
+                sx={{ maxWidth: '100%', borderRadius: 5 }}
+                alt={`${title} logo`}
+                loading='lazy'
+              />
+            </Grid>
+            <Grid sm={8}>
+              <Box className='d-flex flex-column ms-md-3 mt-3 mt-md-0'>
+                <Typography variant='h4' component='h1'>
+                  {title}
+                </Typography>
+                <Typography>
+                  <span className='fw-bold'>Description: </span>
+                  {description}
+                </Typography>
+                <Box marginTop={2}>
+                  <Button
+                    endIcon={<Link />}
+                    variant='contained'
+                    href={webURL}
+                    target='_blank'
+                  >
+                    Website
+                  </Button>
+                  <Button
+                    endIcon={<Link />}
+                    variant='contained'
+                    href={githubURL}
+                    target='_blank'
+                    sx={{
+                      ml: 1,
+                    }}
+                  >
+                    GitHub
+                  </Button>
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
         </CardContent>
       </Card>
     </>
