a1fa43e7f27d61eff225eb2315cb149ece5a3685
Debajit Mallick <mallickdebajit3@gmail.com>
2023-11-15 10:03:58 +0530

    Add firebase-db for home page #118

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -1,14 +1,13 @@
 import { Link, Typography } from '@mui/material';
-import { contactsData } from '../../constants/constant';
 
-const Contact = () => {
+const Contact: React.FC<{ contacts: any[] }> = ({ contacts }) => {
   return (
     <section className='mt-5'>
       <Typography variant='h5' textAlign='center'>
         Contact Me
       </Typography>
       <div className='d-flex mt-3 mb-4 justify-content-around w-50 m-auto'>
-        {contactsData.map((contactData) => (
+        {contacts.map((contactData) => (
           <Link target='_blank' key={contactData.id} href={contactData.url}>
             {contactData.icon}
           </Link>
